e34564746b2652d09c9a3ff2bc70aa27fcdb2c1f
Arnaud Zago <arnaud@zendoc.com>
2020-12-01 19:46:29 +0100

    Initial commit

---
+++ src/day1/day1.ts
@@ -0,0 +1,40 @@
+import {DAY_1} from "../inputs";
+
+export function day1Part1(): number {
+    const RESULT_TO_FIND = 2020;
+    const inputs = DAY_1;
+    let resultA = 0, resultB = 0;
+
+    for (let inputA of inputs) {
+        const inputB = inputs.find(item2 => inputA + item2 === RESULT_TO_FIND);
+        if (!!inputB) {
+            resultA = inputA;
+            resultB = inputB;
+            break;
+        }
+    }
+    console.info('Day1 -> part1 -> resultA', resultA, 'resultB', resultB);
+    return (resultA * resultB);
+}
+
+
+export function day1Part2(): number {
+    const RESULT_TO_FIND = 2020;
+    const inputs = DAY_1;
+    let resultA = 0, resultB = 0, resultC = 0;
+
+    for (let inputA of inputs) {
+        for (let inputB of inputs) {
+            const inputC = inputs.find(item2 => inputA + inputB + item2 === RESULT_TO_FIND);
+            if (!!inputC) {
+                resultA = inputA;
+                resultB = inputB;
+                resultC = inputC;
+                break;
+            }
+        }
+
+    }
+    console.info('Day1 -> part2 -> resultA', resultA, 'resultB', resultB, 'resultC', resultC);
+    return (resultA * resultB * resultC);
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {day1Part1, day1Part2} from "./day1/day1";
+
+console.info('Day 1 -> part1 -> result', day1Part1());
+console.info('Day 1 -> part2 -> result', day1Part2());
