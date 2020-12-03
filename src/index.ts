11bb7e71024b196c1641c1770e40eef0c9bdbe28
Arnaud Zago <arnaud@zendoc.com>
2020-12-03 18:52:45 +0100

    feat(day2): setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import {day1Part1, day1Part2} from "./day1/day1";
+import {DAY2_INPUTS_STRING} from "./day2/day2_inputs";
 
 console.info('Day 1 -> part1 -> result', day1Part1());
 console.info('Day 1 -> part2 -> result', day1Part2());
+
+console.info('Day 2 -> format', formatInputsToArrayString(DAY2_INPUTS_STRING));
+
+
+function formatInputsToArrayString(input: string): Array<string> {
+    return input.split('\n');
+}
+
+
+function formatInputsToArrayNumber(input: string): Array<number> {
+    return input.split('\n').map(item => +item);
+}
