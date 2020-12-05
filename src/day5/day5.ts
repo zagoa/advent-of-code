a3615363b70f34b3319daf7a8c372e6b6592c459
zagoa <arnaud.zago@gmail.com>
2020-12-05 21:32:44 +0100

    feat(day5): part 1 done

---
+++ src/day5/day5.ts
@@ -0,0 +1,58 @@
+export function day5part1(inputs: Array<string>) {
+    const arrayOfIds: Array<number> = inputs.map(input => {
+        const rowCode: string = input.substring(0, 6);
+        const rowSelectCode: string = input.substring(6, 7);
+        const seatCode: string = input.substring(7, 9);
+        const seatSelectCode: string = input.substring(9, 10);
+
+        const row = findRow(rowCode, rowSelectCode, 0, 127);
+        const seat = findSeat(seatCode, seatSelectCode, 0, 7);
+        return row * 8 + seat;
+    });
+
+    return Math.max(...arrayOfIds);
+
+}
+
+function findRow(rowCode: string, selectCode: string, min: number, max: number): number {
+    Array.from(rowCode).forEach((rowOps) => {
+        switch (rowOps) {
+            case 'B':
+                min = Math.ceil(max - ((max - min) / 2))
+                break;
+            case 'F':
+                max = Math.floor((max - min) / 2 + (min))
+                break;
+            default:
+                break;
+        }
+    });
+
+    if (selectCode === 'F') {
+        return Math.min(min, max);
+    } else {
+        return Math.max(min, max);
+    }
+}
+
+
+function findSeat(seatCode: string, selectCode: string, min: number, max: number): number {
+    Array.from(seatCode).forEach((rowOps: string) => {
+        switch (rowOps) {
+            case 'R':
+                min = Math.ceil(max - ((max - min) / 2))
+                break;
+            case 'L':
+                max = Math.floor((max - min) / 2 + (min))
+                break;
+            default:
+                break;
+        }
+    });
+
+    if (selectCode === 'L') {
+        return Math.min(min, max);
+    } else {
+        return Math.max(min, max);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,28 @@ import {day3Part1, day3Part2} from "./day3/day3";
 import {DAY3_INPUTS, TEST_EXAMPLE_DAY3} from "./day3/day3_inputs";
 import {day4Part1, day4Part2} from "./day4/day4";
 import {DAY4_INPUTS, DAY4_TEST} from "./day4/day4_inputs";
+import {day5part1} from "./day5/day5";
+import {DAY5_INPUTS, DAY5_TEST} from "./day5/day5_inputs";
 
+console.info("#############################################");
 console.info('Day 1 -> part1 -> result', day1Part1());
 console.info('Day 1 -> part2 -> result', day1Part2());
 
+console.info("#############################################");
 console.info('Day 2 -> part1 -> result', day2Part1());
 console.info('Day 2 -> part1 -> result', day2Part2());
 
+console.info("#############################################");
 console.info('Day 3 -> part1 -> test must be 7', day3Part1(TEST_EXAMPLE_DAY3));
 console.info('Day 3 -> part1 -> result', day3Part1(DAY3_INPUTS));
-
 console.info('Day 3 -> part2 -> test must be 336', day3Part2(TEST_EXAMPLE_DAY3));
 console.info('Day 3 -> part2 -> result', day3Part2(DAY3_INPUTS));
 
+console.info("#############################################");
 console.info('Day 4 -> part1 -> test must be 2', day4Part1(DAY4_TEST));
 console.info('Day 4 -> part1 -> result', day4Part1(DAY4_INPUTS));
 console.info('Day 4 -> part2 -> result', day4Part2(DAY4_INPUTS));
+
+console.info("#############################################");
+console.info('Day 5 -> part1 -> test expect 820', day5part1(DAY5_TEST));
+console.info('Day 5 -> part1 -> result', day5part1(DAY5_INPUTS));
