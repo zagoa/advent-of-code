4a7d9b25881038b975175c6e67c1c864c9afec4c
zagoa <arnaud.zago@gmail.com>
2020-12-06 11:47:07 +0100

    feat(day6): easy day

---
+++ src/day6/day6.ts
@@ -0,0 +1,38 @@
+export function day6part1(inputs: Array<string>): number {
+    return inputs.reduce((count, input) => {
+        const mapOfResponses: Map<string, number> = new Map<string, number>();
+
+        Array.from(input).forEach(letter => {
+            if (letter !== '\n') {
+                if (mapOfResponses.has(letter)) {
+                    mapOfResponses.set(letter, mapOfResponses.get(letter) + 1);
+                } else {
+                    mapOfResponses.set(letter, 1)
+                }
+            }
+        })
+
+        return count + mapOfResponses.size;
+    }, 0)
+}
+
+
+export function day6part2(inputs: Array<string>): number {
+    return inputs.reduce((count, input) => {
+        const mapOfResponses: Map<string, number> = new Map<string, number>();
+        const peopleResponses: Array<string> = input.split('\n');
+        peopleResponses.forEach(peopleResponse => {
+            Array.from(peopleResponse).forEach(letter => {
+                if (mapOfResponses.has(letter)) {
+                    mapOfResponses.set(letter, mapOfResponses.get(letter) + 1);
+                } else {
+                    mapOfResponses.set(letter, 1)
+                }
+            })
+        })
+
+
+
+        return count +  (Array.from(mapOfResponses.values()).filter(numberResponse => numberResponse === peopleResponses.length)).length;
+    }, 0)
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {day4Part1, day4Part2} from "./day4/day4";
 import {DAY4_INPUTS, DAY4_TEST} from "./day4/day4_inputs";
 import {day5part1, day5part2} from "./day5/day5";
 import {DAY5_INPUTS, DAY5_TEST} from "./day5/day5_inputs";
+import {day6part1, day6part2} from "./day6/day6";
+import {DAY6_INPUTS} from "./day6/day6_inputs";
 
 console.info("#############################################");
 console.info('Day 1 -> part1 -> result', day1Part1());
@@ -30,3 +32,7 @@ console.info("#############################################");
 console.info('Day 5 -> part1 -> test expect 820', day5part1(DAY5_TEST));
 console.info('Day 5 -> part1 -> result', day5part1(DAY5_INPUTS));
 console.info('Day 5 -> part2 -> result', day5part2(DAY5_INPUTS));
+
+console.info("#############################################");
+console.info('Day 6 -> part1 -> result', day6part1(DAY6_INPUTS));
+console.info('Day 6 -> part2 -> result', day6part2(DAY6_INPUTS));
